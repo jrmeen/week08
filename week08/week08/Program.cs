6433787b56dff5ccbae058f41a785d01c4b4f465
meen <59030143@kmitl.ac.th>
2017-11-26 00:04:18 +0700

    w08p03

--- week08/week08/Program.cs
+++ week08/week08/Program.cs
@@ -8,14 +8,14 @@ namespace ConsoleAppArray
         static void Main(string[] args)
         {
         	int[] data = new int[10];
-        	data[0-] = 0;
+        	
         	data[1] = 1;
         	data[9] = 9;
-        	data[10] = 10;
-        	Console.WriteLine(data[-1]);
+        	
+        	
         	Console.WriteLine(data[1]);
         	Console.WriteLine(data[9]);
-        	Console.WriteLine(data[10]);
+        	
         	Console.ReadLine();
         }
     }
